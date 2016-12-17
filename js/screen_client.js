298e2eacb42dda3a5bc6642109a27678024bbbde
Sipi S <sipi@sipi>
2016-12-17 16:48:06 +0200

    UI updates (event date tags, sodexo menu), refactoring

--- js/screen_client.js
+++ js/screen_client.js
@@ -1,98 +1,81 @@
-//var date_global = new Date();
-var vituttaa_amount = 0;
+var INTERVAL_SEC = 1000; //one-second interval
+var INTERVAL_MIN = 60 * 1000; //one-minute interval
+var INTERVAL_HOUR = 60 * INTERVAL_MIN; //one-hour interval
 
-function displayEvents(){
-	var temp_container = '';
-	$('#eventdummy .briefEventListing').each(function(i){
-		var titlerow = $(this).find( '.eventTitle' ).text().trim().split(' @ ');
-		var meta = $(this).find( '.eventMeta' ).text().trim();
-		var title = titlerow[0];
-		var location = titlerow[1];
-		var date = wdToEnglish(meta.substring(0, 9)); //ma xx.yy. -> Mon xx.yy.
+//To prevent weird behavior, reload the whole page every now and then
+var pageReloadTimer = setInterval(function(){location.reload();}, 6 * INTERVAL_HOUR);
 
-		var hours = meta.substring(10, 19);
-		if(hours.substring(0,3) === 'klo'){
-			date += hours.substring(3);
-		}
 
-		var signup = '';
-		var probe = $.grep(getSignups(), function(e){ return e.title === title; });
-		if (probe.length == 1) {
-			signup = ' <i class="icon-group"></i> '+probe[0].amount;
-		}
+/*
+	"MAIN"
+*/
+$(document).ready(function(){
 
-		var desc = '';
-		if(getLabel(date) !== 'later'){
-			title += getLabel(date);
-			var time = 'All day';
-			var descPart = meta.substring(9);
-			if(date.length > 10){
-				time = date.substring(10);
-				descPart = meta.substring(19);
-			}
-			date = time;
-			desc = '<p class="descrow">'+descPart+'</p>';
-		}
+	// START CLOCK
+	loadClock();
+	var clockTimer = setInterval(function(){
+		loadClock();
+	}, INTERVAL_SEC);
 
-		var tab = '<span class="wide-space"> </span>';
-		var specs = '<i class="icon-time icon-large"></i> '+date+tab+
-		' <i class="icon-map-marker icon-large"></i> '+location+tab+signup;
 
-		temp_container += '<div class="evtItem pure-g"><div class="pure-u-1"><h2>	'+title+
-			'</h2></div><div class="pure-u-1"><h3 class="specrow">'+
-			specs+'</h3>'+desc+'</div></div>';
+	// OUTSIDE TEMPERATURE
+	var weather_url = 'http://outside.aalto.fi/data.tt';
+	loadWeather(weather_url);
+	var weatherTimer = setInterval(function() {
+		loadWeather(weather_url);
+	}, INTERVAL_MIN * 10);
+
+
+	// SODEXO TODAY'S MENU
+	var sodexo_url = getSodexoUrl();
+	loadSodexo(sodexo_url);
+
+
+	// BUS STOPS
+	loadBusStops(hslaccount.username, hslaccount.passphrase);
+	var busTimer = setInterval(function(){
+		loadBusStops(hslaccount.username, hslaccount.passphrase);
+	}, INTERVAL_MIN);
+
+
+
+	// TIETOKILTA EVENTS
+	var event_url = 'http://tietokilta.fi/tapahtumat #pageWrapper'; //note the selector
+	$('#eventdummy').load(event_url, function(){
+		displayEvents();
 	});
-	$('#eventContainer').html(temp_container);
-}
+	var eventTimer = setInterval(function() {
+		$('#eventdummy').load(event_url, function(){
+			displayEvents();
+		});
+	}, INTERVAL_HOUR);
 
-function wdToEnglish(str){
-	var fiEn = {'ma' : 'Mon',
-		    'ti' : 'Tue',
-		    'ke' : 'Wed',
-		    'to' : 'Thu',
-		    'pe' : 'Fri',
-		    'la' : 'Sat',
-		    'su' : 'Sun'};
 
-	return fiEn[str.substring(0,2)]+' '+str.substring(3);
-}
+		// NETHACK SCORES
+/*
+	var nethack_log_url = 'http://www.niksula.hut.fi/~lindhj1/nethack_log.txt';
+	var nethack_scores_url = 'http://www.niksula.hut.fi/~lindhj1/nethack_scores.txt';
+	$('#nethackdummy_log').load(nethack_log_url, function() {
+		$('#nethackdummy_rec').load(nethack_scores_url, function () {
+			displayNethack();
+		});});
+	var nethackTimer = setInterval(function() {
+		$('#nethackdummy_log').load(nethack_log_url, function() {
+			$('#nethackdummy_rec').load(nethack_scores_url, function () {
+				displayNethack();
+			});});
+	}, INTERVAL_MIN * 15);
+*/
 
-function getLabel(dateStr){
-	var now = new Date();
-	var dateArr = dateStr.substring(4).split('.');
-	var tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
 
-	// check if date is today
-	if(now.getDate() == dateArr[0] && (now.getMonth()+1) == dateArr[1]){
-		return '<div class="labeltag">TODAY</div>';
-	}
-	// or tomorrow
-	else if(tomorrow.getDate() == dateArr[0] && (tomorrow.getMonth()+1) == dateArr[1]){
-		return '<div class="labeltag">TOMORROW</div>';
-	}
-	else {
-		return 'later';
-	}
-}
+});
 
-function getHours(str){
-	return;
-}
 
-function getSignups(){
-	var signups = [];
 
-	$('#eventdummy .briefSignupListing').each(function(){
-		var title = $(this).children('.signupTitle').text().trim();
-		var amount = $(this).children('.signupMeta').text().trim().substring(17);
-		var obj = {'title' : title, 'amount' : amount};
-		signups.push(obj);
-	});
 
-	return signups;
-
-}
-
+/*
+	CLOCK & DATE
+*/
 function loadClock(){
 	var clock = new Date();
 	var hours = clock.getHours();
@@ -118,16 +101,47 @@ function loadClock(){
 	$('#dateDisp').text(wday+' '+date);
 }
 
-function loadWeather(url){
 
+/*
+	OUTSIDE TEMPERATURE
+*/
+function loadWeather(url){
 	$.get(url, function(raw) {
 		var data = JSON.parse(raw);
 		$('#weather').text(Math.round(data["gent-outside-t"]) + ' °C');
 	});
+}
 
+
+/*
+	SODEXO & SUBWAY MENUS
+*/
+function loadSodexo(url){
+	$.get(url, function(data){
+		for(var i=0; i < data.courses.length; i++){
+				var entry =  '<div class="sodexoItem">';
+				entry += '<h4>'+data.courses[i].title_fi+'</h4>';
+				entry += '<h4>'+data.courses[i].title_en+'</h4>';
+				entry += '</div>';
+				$('#sodexoContainer').append(entry);
+		}
+	});
 
 }
 
+function getSodexoUrl(){
+	var base = 'http://www.sodexo.fi/ruokalistat/output/daily_json/142/';
+	var d = new Date();
+    var y = d.getFullYear();
+    var m = d.getMonth() + 1;
+    var day = d.getDate();
+    return base + y+'/'+m+'/'+day+'/fi';
+}
+
+
+/*
+	BUS STOPS
+*/
 function cleanBusCode(long_code) {
 	var short_code = long_code.slice(1).split(" ")[0];
 	if ( short_code[0] == "0" ) {
@@ -168,7 +182,7 @@ function callHSL(api_url, busstop_id, element_id) {
 		var times = [];
 		var timetable = "<table>";
 		$(deps).each(function( index ) {
-			console.log(this);
+			//console.log(this);
 			var buscode = cleanBusCode(this.code);
 			var timecode = cleanTimeCode(this.time);
 			var row = "<tr><td>" + timecode + "</td><td>" + buscode + "</td></tr>";
@@ -184,6 +198,106 @@ function callHSL(api_url, busstop_id, element_id) {
 	});
 }
 
+
+
+
+/*
+	GUILD EVENTS
+*/
+function displayEvents(){
+
+	var eventItems = '';
+
+	$('#eventdummy .briefEventListing').each(function(i){
+		var titlerow = $(this).find('.eventTitle').text().trim().split(' @ ');
+		var meta = $(this).find('.eventMeta').text().trim();
+		var title = titlerow[0];
+		var location = titlerow[1];
+		var date = wdToEnglish(meta.substring(0, 9)); //ma xx.yy. -> Mon xx.yy.
+
+		var hours = meta.substring(10, 19);
+		if(hours.substring(0,3) === 'klo'){
+			date += hours.substring(3);
+		}
+
+		var signup = '';
+		var probe = $.grep(getSignups(), function(e){ return e.title === title; });
+		if (probe.length == 1) {
+			signup = ' <i class="fa fa-group"></i> '+probe[0].amount;
+		}
+
+		var desc = '';
+		var label = getLabel(date);
+		
+		if(label !== 'later'){ //show desc if today or tommorow
+			var time = 'All day';
+			var descPart = meta.substring(9);
+			if(date.length > 10){
+				time = date.substring(10);
+				descPart = meta.substring(19);
+			}
+			date = label + ' ' + time;
+			desc = '<p class="descrow">'+descPart+'</p>';
+		}
+
+		var tab = '<span class="wide-space"></span>';
+		var specs = '<i class="fa fa-clock-o fa-lg"></i> '+date+tab+
+		' <i class="fa fa-map-marker fa-lg"></i> '+location+tab+signup;
+
+		eventItems += '<div class="evtItem pure-g"><div class="pure-u-1"><h2>	'+title+
+			'</h2></div><div class="pure-u-1"><h3 class="specrow">'+
+			specs+'</h3>'+desc+'</div></div>';
+	});
+
+	$('#eventContainer').html(eventItems);
+
+	function wdToEnglish(str){
+	var fiEn = { 'ma' : 'Mon',
+		    'ti' : 'Tue',
+		    'ke' : 'Wed',
+		    'to' : 'Thu',
+		    'pe' : 'Fri',
+		    'la' : 'Sat',
+		    'su' : 'Sun' };
+	return fiEn[str.substring(0,2)]+' '+str.substring(3);
+	}
+
+	function getLabel(dateStr){
+		var now = new Date();
+		var dateArr = dateStr.substring(4).split('.');
+		var tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+
+		// check if date is today
+		if(now.getDate() == dateArr[0] && (now.getMonth()+1) == dateArr[1]){
+			// return '<div class="labeltag">TODAY</div>'; //old today-tag
+			return '<b>TODAY</b>';
+		}
+		// or tomorrow
+		else if(tomorrow.getDate() == dateArr[0] && (tomorrow.getMonth()+1) == dateArr[1]){
+			//return '<div class="labeltag">TOMORROW</div>'; //old tomorrow-tag
+			return '<b>TOMORROW</b>';
+		}
+		else {
+			return 'later';
+		}
+	}
+
+	function getSignups(){
+		var signups = [];
+		$('#eventdummy .briefSignupListing').each(function(){
+			var title = $(this).children('.signupTitle').text().trim();
+			var amount = $(this).children('.signupMeta').text().trim().substring(17);
+			var obj = {'title' : title, 'amount' : amount};
+			signups.push(obj);
+		});
+		return signups;
+	}
+
+}
+
+
+//Legacy: nethack scores
+/*
 function displayNethack() {
 	var nethack_log = $('#nethackdummy_log').text();
 	var nethack_scores = $('#nethackdummy_rec').text();
@@ -191,101 +305,4 @@ function displayNethack() {
 	var title_2 = '<div class="nethacktitle"><p>LAST 5 GAMES<p></div>';
 	$('#nethackContainer').html(title_1+'<pre>'+nethack_scores+'</pre>'+title_2+'<pre>'+nethack_log+'</pre>');
 }
-
-
-function loadSodexo(url){
-
-	$.get(url, displaySodexo);
-
-}
-
-function displaySodexo(data){
-	for(var i=0; i < data.courses.length; i++){
-			var entry =  '<div class="sodexoItem">';
-			entry += '<h3>'+data.courses[i].title_fi;
-			entry += '<br>'+data.courses[i].title_en+'</h3>';
-			entry += '</div>';
-			$('#sodexoContainer').append(entry);
-	}
-}
-
-function getSodexoUrl(){
-
-	var base = 'http://www.sodexo.fi/ruokalistat/output/daily_json/142/';
-	var d = new Date();
-    var y = d.getFullYear();
-    var m = d.getMonth() +1;
-    var day = d.getDate();
-
-    return base + y+'/'+m+'/'+day+'/fi';
-
-}
-
-
-
-$(document).ready(function(){
-
-	var INTERVAL_SEC = 1000; //one-second interval
-	var INTERVAL_MIN = 60 * 1000; //one-minute interval
-	var INTERVAL_HOUR = 60 * INTERVAL_MIN; //one-hour interval
-
-	//To prevent weird behavior, reload the whole page every now and then
-	var pageReloadTimer = setInterval(function(){location.reload();}, 6 * INTERVAL_HOUR);
-
-	loadClock();
-	var clockTimer = setInterval(function(){
-		loadClock();
-	}, INTERVAL_SEC);
-
-
-	//OUTSIDE TEMPERATURE
-	var weather_url = 'http://outside.aalto.fi/data.txt';
-	loadWeather(weather_url);
-	var weatherTimer = setInterval(function() {
-		loadWeather(weather_url);
-	}, INTERVAL_MIN * 10);
-
-
-	//SODEXO TODAY'S MENU
-	var sodexo_url = getSodexoUrl();
-	loadSodexo(sodexo_url);
-
-
-	//BUS STOPS
-	loadBusStops(hslaccount.username, hslaccount.passphrase);
-	var busTimer = setInterval(function(){
-		loadBusStops(hslaccount.username, hslaccount.passphrase);
-	}, INTERVAL_MIN);
-
-
-
-	//TIETOKILTA EVENTS
-	var event_url = 'http://tietokilta.fi/tapahtumat #pageWrapper'; //note the selector
-	$('#eventdummy').load(event_url, function(){
-		displayEvents();
-	});
-	var eventTimer = setInterval(function() {
-		$('#eventdummy').load(event_url, function(){
-			displayEvents();
-		});
-	}, INTERVAL_HOUR);
-
-
-		//NETHACK SCORES
-/*
-	var nethack_log_url = 'http://www.niksula.hut.fi/~lindhj1/nethack_log.txt';
-	var nethack_scores_url = 'http://www.niksula.hut.fi/~lindhj1/nethack_scores.txt';
-	$('#nethackdummy_log').load(nethack_log_url, function() {
-		$('#nethackdummy_rec').load(nethack_scores_url, function () {
-			displayNethack();
-		});});
-	var nethackTimer = setInterval(function() {
-		$('#nethackdummy_log').load(nethack_log_url, function() {
-			$('#nethackdummy_rec').load(nethack_scores_url, function () {
-				displayNethack();
-			});});
-	}, INTERVAL_MIN * 15);
 */
-
-
-});
