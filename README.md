98c950b75db2ecb72a250ac7fbc89e89d4216788
Sipi S <sipi@sipi>
2016-12-18 15:46:01 +0200

    remove nethack scores

--- README.md
+++ README.md
@@ -3,14 +3,13 @@ Tikscreen
 
 Infoscreen for Tietokilta (CS Guild) guild room.
 
-UPDATE 2017-01-01: lipsum
+*UPDATE 2017-01-01:* 
+Fetch bus timetables from new HSL API (Digitransit), UI updates & some refactoring
 
 
 ### Deployment
 
-__DISCLAIMER: No warranties, etc.__
-
-__Also, this is a locally-run demo solution intended to run client-only in a fresh browser dedicated to the infoscreen. The page makes calls to multiple providers, many without decent APIs or CORS headers. For serious production environments, do not run browser with same-origin security turned off.__
+**These instructions describe a local demo solution intended to run client-only in a fresh browser dedicated to the infoscreen. The page makes calls to multiple providers, many without decent APIs or CORS headers. For serious production environments, do not run browser with same-origin security turned off.**
 
 Start a simple HTTP server to serve files from tikscreen folder OR open _index.html_ directly in browser with access to local files and cross-origin security disabled.
 
@@ -21,3 +20,7 @@ chromium-browser --disable-web-security --user-data-dir=<dir> --start-fullscreen
 ```
 
 The user-data-dir should be a new Chrome profile folder that is preferrably not used for anything else.
+
+### License
+
+No warranties, provided as-is, etc... Feel free to copy and use as you like, but please adhere to possible terms of the third-party APIs! The guild website is also under copyright of Tietokilta ry.
--- js/screen_client.js
+++ js/screen_client.js
@@ -1,3 +1,5 @@
+
+//Define some time intervals
 var INTERVAL_SEC = 1000; //one-second interval
 var INTERVAL_MIN = 60 * 1000; //one-minute interval
 var INTERVAL_HOUR = 60 * INTERVAL_MIN; //one-hour interval
@@ -19,7 +21,7 @@ $(document).ready(function(){
 
 
 	// OUTSIDE TEMPERATURE
-	var weather_url = 'http://outside.aalto.fi/data.tt';
+	var weather_url = 'http://outside.aalto.fi/data.txt';
 	loadWeather(weather_url);
 	var weatherTimer = setInterval(function() {
 		loadWeather(weather_url);
@@ -39,7 +41,8 @@ $(document).ready(function(){
 
 
 
-	// TIETOKILTA EVENTS
+	// TIETOKILTA EVENTS 
+	//TODO loadEvents
 	var event_url = 'http://tietokilta.fi/tapahtumat #pageWrapper'; //note the selector
 	$('#eventdummy').load(event_url, function(){
 		displayEvents();
@@ -50,26 +53,7 @@ $(document).ready(function(){
 		});
 	}, INTERVAL_HOUR);
 
-
-		// NETHACK SCORES
-/*
-	var nethack_log_url = 'http://www.niksula.hut.fi/~lindhj1/nethack_log.txt';
-	var nethack_scores_url = 'http://www.niksula.hut.fi/~lindhj1/nethack_scores.txt';
-	$('#nethackdummy_log').load(nethack_log_url, function() {
-		$('#nethackdummy_rec').load(nethack_scores_url, function () {
-			displayNethack();
-		});});
-	var nethackTimer = setInterval(function() {
-		$('#nethackdummy_log').load(nethack_log_url, function() {
-			$('#nethackdummy_rec').load(nethack_scores_url, function () {
-				displayNethack();
-			});});
-	}, INTERVAL_MIN * 15);
-*/
-
-
 });
-
 
 
 
@@ -85,14 +69,14 @@ function loadClock(){
 
 	var time = hours + ':' + mins;
 
-	var weekday=new Array(7);
-	weekday[0]="Sunday";
-	weekday[1]="Monday";
-	weekday[2]="Tuesday";
-	weekday[3]="Wednesday";
-	weekday[4]="Thursday";
-	weekday[5]="Friday";
-	weekday[6]="Saturday";
+	var weekday = new Array(7);
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
 
 	var wday = weekday[clock.getDay()];
 
@@ -119,11 +103,11 @@ function loadWeather(url){
 function loadSodexo(url){
 	$.get(url, function(data){
 		for(var i=0; i < data.courses.length; i++){
-				var entry =  '<div class="sodexoItem">';
-				entry += '<h4>'+data.courses[i].title_fi+'</h4>';
-				entry += '<h4>'+data.courses[i].title_en+'</h4>';
-				entry += '</div>';
-				$('#sodexoContainer').append(entry);
+			var entry =  '<div class="sodexoItem">';
+			entry += '<h4>'+data.courses[i].title_fi+'</h4>';
+			entry += '<h4>'+data.courses[i].title_en+'</h4>';
+			entry += '</div>';
+			$('#sodexoContainer').append(entry);
 		}
 	});
 
@@ -132,16 +116,31 @@ function loadSodexo(url){
 function getSodexoUrl(){
 	var base = 'http://www.sodexo.fi/ruokalistat/output/daily_json/142/';
 	var d = new Date();
-    var y = d.getFullYear();
-    var m = d.getMonth() + 1;
-    var day = d.getDate();
-    return base + y+'/'+m+'/'+day+'/fi';
+	var y = d.getFullYear();
+	var m = d.getMonth() + 1;
+	var day = d.getDate();
+	return base + y+'/'+m+'/'+day+'/fi';
 }
 
 
 /*
 	BUS STOPS
 */
+function loadBusStops(api_account, api_password){
+	/* //OLD
+	var currentWeekday = new Date(new Date().getTime() - 2 * 60 * 60 * 1000 ).getDay();
+	var id_alvaraalto_hki = "2222235";
+	var id_alvaraalto_esp = "2222211";
+	var api_url = "http://api.reittiopas.fi/hsl/prod/?user="+api_account+"&pass="+api_password;
+	callHSL(api_url, id_alvaraalto_esp, "nw");
+	callHSL(api_url, id_alvaraalto_hki, "se");
+	*/
+
+
+
+}
+
+
 function cleanBusCode(long_code) {
 	var short_code = long_code.slice(1).split(" ")[0];
 	if ( short_code[0] == "0" ) {
@@ -151,30 +150,21 @@ function cleanBusCode(long_code) {
 }
 
 function cleanTimeCode(old_code){
-
 	old_code = old_code.toString();
-
-	if(old_code.length < 4)
+	if(old_code.length < 4){
 		old_code = '0'+old_code;
-
+	}
 	return old_code.substr(0, 2)%24 + ':' + old_code.substr(2);
 }
 
-function loadBusStops(api_account, api_password){
-	var currentWeekday = new Date(new Date().getTime() - 2 * 60 * 60 * 1000 ).getDay();
-	var id_alvaraalto_hki = "2222235";
-	var id_alvaraalto_esp = "2222211";
-	var api_url = "http://api.reittiopas.fi/hsl/prod/?user="+api_account+"&pass="+api_password;
-	callHSL(api_url, id_alvaraalto_esp, "nw");
-	callHSL(api_url, id_alvaraalto_hki, "se");
-}
+
 
 function callHSL(api_url, busstop_id, element_id) {
 	var target = $("#seBusInfo");
 	if (element_id == "nw")
 		target = $("#nwBusInfo");
 
-    var TIME_LIMIT = "240"; //max minutes to future
+	var TIME_LIMIT = "240"; //max minutes to future
 	var DEP_LIMIT = "10"; //max number of departures to fetch
 
 	$.getJSON(api_url+"&request=stop&code="+busstop_id+"&time_limit="+TIME_LIMIT+"&dep_limit="+DEP_LIMIT, function(data) {
@@ -253,12 +243,12 @@ function displayEvents(){
 
 	function wdToEnglish(str){
 	var fiEn = { 'ma' : 'Mon',
-		    'ti' : 'Tue',
-		    'ke' : 'Wed',
-		    'to' : 'Thu',
-		    'pe' : 'Fri',
-		    'la' : 'Sat',
-		    'su' : 'Sun' };
+			'ti' : 'Tue',
+			'ke' : 'Wed',
+			'to' : 'Thu',
+			'pe' : 'Fri',
+			'la' : 'Sat',
+			'su' : 'Sun' };
 	return fiEn[str.substring(0,2)]+' '+str.substring(3);
 	}
 
@@ -295,14 +285,3 @@ function displayEvents(){
 
 }
 
-
-//Legacy: nethack scores
-/*
-function displayNethack() {
-	var nethack_log = $('#nethackdummy_log').text();
-	var nethack_scores = $('#nethackdummy_rec').text();
-	var title_1 = '<div class="nethacktitle"><p>NETHACK TOP 5<p></div>';
-	var title_2 = '<div class="nethacktitle"><p>LAST 5 GAMES<p></div>';
-	$('#nethackContainer').html(title_1+'<pre>'+nethack_scores+'</pre>'+title_2+'<pre>'+nethack_log+'</pre>');
-}
-*/
